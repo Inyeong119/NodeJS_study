e2dae0a090f0b9a3a2f51cff12ee34f643a8b8a7
Inyeong Jung <inyeong.jung@pwc.com>
2025-07-25 20:56:56 +0900

    백엔드까지 배포버전 1 업로드

--- todo-backend/app.js
+++ todo-backend/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
-
+require('dotenv').config();
+const MONGO_DB_PROD = process.env.MONGO_DB_PROD;
+console.log("MONGO_DB_PROD",MONGO_DB_PROD);
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/api",indexRouter);
 
-const mongoURI = 'mongodb://localhost:27017/todo-demo';
+const mongoURI = MONGO_DB_PROD;
 
 mongoose.connect(mongoURI,{useNewURLParser:true}).then(()=>{
   console.log("mongoose connected");
@@ -16,6 +20,6 @@ mongoose.connect(mongoURI,{useNewURLParser:true}).then(()=>{
     console.log("DB Connection fail",err);
   });
 
-app.listen(5000,()=>{
+app.listen(process.env.PORT || 5000,()=>{
   console.log("server on 5000");
 })
